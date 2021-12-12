659601703756471a521a975bc25fcc19b8556d05
Penneey <developer.p@nug-mm.net>
2021-12-12 23:10:51 +0800

    Update the latest mock and made the changes in the component.

--- client/app/components/order/OrderDetail.jsx
+++ client/app/components/order/OrderDetail.jsx
@@ -53,98 +53,116 @@ export default class OrderDetail extends React.Component {
 
   render() {
     let minDate = Moment().subtract(29, "days")
-                          .hour(0)
-                          .minute(0)
-                          .second(0)
-                          .millisecond(0)
-                          .toDate();
+      .hour(0)
+      .minute(0)
+      .second(0)
+      .millisecond(0)
+      .toDate();
     let maxDate = Moment().hour(23)
-                          .minute(59)
-                          .second(59)
-                          .millisecond(999)
-                          .toDate();
+      .minute(59)
+      .second(59)
+      .millisecond(999)
+      .toDate();
     let startDate = Moment().format("MM/DD/YYYY");
-    
+    let currencyArr = ["USD", "EUR", "SGD", "MMK"];
+
     return (
       <React.Fragment>
         <div className="header-portal">
-            <div className="top-header-portal">Agent Portal</div>
-            <div className="bottom-header-portal">United Bonds</div>
+          <div className="top-header-portal">Agent Portal</div>
+          <div className="bottom-header-portal">United Bonds</div>
         </div>
-        <div className="body-portal">
-            <div className="bond-order-header">Information about bond</div>
-            <div className="bond-info-row1">
-                <div className="bond-info-row1-name">
-                    <div className="buyer-name-info">Buyer Name</div>
-                    <div className="buyer-name-info">
-                        <input className="input-style" type="text" id="buyerName" />
-                    </div>
-                </div>
-                <div className="bond-info-row1-date">
-                    <div className="payment-date-info">Date of Payment</div>
-                    <div className="payment-date-info">
-                        <DateTimePicker minDate={minDate} maxDate={maxDate} startDate={startDate} classes="input-style" />
-                    </div>
-                </div>
+        <div className="main-portal">
+          <div className="bond-order-header">Information about bond</div>
+          <div className="bond-info-row1">
+            <div className="bond-info-row1-name">
+              <div className="buyer-name-info">Buyer Name</div>
+              <div className="buyer-name-info">
+                <input className="input-style" type="text" />
+              </div>
             </div>
-            <div className="bond-info-row2">
-                <div className="buyer-email-style">Buyer Email</div>
-                <div className="buyer-email-style">
-                    <input className="input-style" type="text" id="email" required
-                        oninvalid="this.setCustomValidity('User ID is a must')" />
-                </div>
+            <div className="bond-info-row1-date">
+              <div className="payment-date-info">Date of Payment</div>
+              <div className="payment-date-info">
+                <DateTimePicker minDate={minDate} maxDate={maxDate} startDate={startDate} classes="input-style" />
+              </div>
             </div>
-            <div className="bond-info-row3">
-                <div className="bond-selection-header">Bond Quantities</div>
-                <div className="bond-selection">
-                    <div className="bond-selection-single">
-                        <div className="single-selection-info">$ 100 Bond</div>
-                        <div className="single-selection-info">
-                            <input className="input-style" type="number" id="quantity" name="quantity" min="0" />
-                        </div>
-                    </div>
-                    <div className="bond-selection-single">
-                        <div className="single-selection-info">$ 500 Bond</div>
-                        <div className="single-selection-info">
-                            <input className="input-style" type="number" id="quantity" name="quantity" min="0" />
-                        </div>
-                    </div>
-                    <div className="bond-selection-single">
-                        <div className="single-selection-info">$ 1000 Bond</div>
-                        <div className="single-selection-info">
-                            <input className="input-style" type="number" id="quantity" name="quantity" min="0" />
-                        </div>
-                    </div>
-                    <div className="bond-selection-single">
-                        <div className="single-selection-info">$ 5000 Bond</div>
-                        <div className="single-selection-info">
-                            <input className="input-style" type="number" id="quantity" name="quantity" min="0" />
-                        </div>
-                    </div>
-                </div>
-                <div className="total-amount-selection">
-                    <div className="total-amount-single">
-                        <div className="total-amount-info">Total Amount in USD</div>
-                        <div className="total-amount-info">
-                            <input className="input-style" type="text" id="totalAmount" value="1000" disabled />
-                        </div>
-                    </div>
-                    <div className="total-amount-single">
-                        <div className="total-amount-info">Total Amount Received</div>
-                        <div className="total-amount-info">
-                            <input className="input-style" value="1000" type="text" id="amountReceived" disabled />
-                        </div>
-                    </div>
-                    <div className="total-currencycode">
-                        <div className="currencycode-info"></div>
-                        <select className="currencycode-info" name="cars" id="currencyList"></select>
-                    </div>
-                </div>
+          </div>
+          <div className="bond-info-row2">
+            <div className="buyer-email-style">Buyer Email</div>
+            <div className="buyer-email-style">
+              <input className="input-style" type="text" />
             </div>
-            <div className="bond-info-row4">
-                <div className="cancel-button">Cancel</div>
-                <div className="submit-button" onclick="onSubmit()">Submit</div>
+          </div>
+          <div className="bond-info-row3">
+            <div className="bond-selection-header">Bond Quantities</div>
+            <div className="bond-selection">
+              <div className="bond-selection-single">
+                <div className="single-selection-info">$ 100 Bond</div>
+                <div className="single-selection-info">
+                  <input className="input-style" type="number" min="0" />
+                </div>
+              </div>
+              <div className="bond-selection-single">
+                <div className="single-selection-info">$ 500 Bond</div>
+                <div className="single-selection-info">
+                  <input className="input-style" type="number" min="0" />
+                </div>
+              </div>
+              <div className="bond-selection-single">
+                <div className="single-selection-info">$ 1000 Bond</div>
+                <div className="single-selection-info">
+                  <input className="input-style" type="number" min="0" />
+                </div>
+              </div>
+              <div className="bond-selection-single">
+                <div className="single-selection-info">$ 5000 Bond</div>
+                <div className="single-selection-info">
+                  <input className="input-style" type="number" min="0" />
+                </div>
+              </div>
             </div>
+            <div className="total-amount-selection">
+              <div className="bond-value-info">
+                <div className="single-selection-info">Bond Value in USD</div>
+                <div className="single-selection-info">
+                  <input className="input-style" type="text" value="1000" disabled />
+                </div>
+              </div>
+              <div className="total-currencycode">
+                <div className="currencycode-info"></div>
+                <select className="currencycode-info">
+                  {currencyArr.map((code, index) => {
+                    return (<option key={index} value={code} >{code}</option>)
+                  })}
+                </select>
+              </div>
+              <div className="exhange-rate-info">
+                <div className="currencycode-info">Exchange Rate</div>
+                <div className="currencycode-info">
+                  <input className="input-style" type="text" value="1350" disabled />
+                </div>
+              </div>
+            </div>
+            <div className="total-amount-selection">
+              <div className="total-amount-single-left">
+                <div className="total-amount-info">Bond Value in Selected Currency</div>
+                <div className="total-amount-info">
+                  <input className="input-style" type="text" value="1000" disabled />
+                </div>
+              </div>
+              <div className="total-amount-single-right">
+                <div className="total-amount-info">Actual Amount in Selected Currency</div>
+                <div className="total-amount-info">
+                  <input className="input-style" value="1000" type="text" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bond-info-row4">
+            <div className="cancel-button">Cancel</div>
+            <div className="submit-button">Submit</div>
+          </div>
         </div>
       </React.Fragment>
     );
