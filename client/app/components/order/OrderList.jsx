a120e0ed11b3909560d1fcc71e0f9a49b32092cc
mgp.spring.2021 <mgp.spring.2021@gmail.com>
2021-12-21 19:28:56 +0800

    order list minor changes

--- client/app/components/order/OrderList.jsx
+++ client/app/components/order/OrderList.jsx
@@ -15,147 +15,15 @@ export default class OrderList extends React.Component {
     super();
   }
 
-  _getSampleData() {
-    return [
-      {
-        "buyerName": "mg p",
-        "orderID": "mgp@mail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "100",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "nz",
-        "orderID": "nz@mail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "600",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "panny",
-        "orderID": "panny@mail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "5000",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "hein",
-        "orderID": "hein@mail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "5600",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "tz",
-        "orderID": "tz@mail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "10000",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "jeremy",
-        "orderID": "jeremy@ewedcmail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "2100",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "jeredfmy",
-        "orderID": "jeremghr@mail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "2100",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "jferemy",
-        "orderID": "jeremyas@mail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "2100",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "jerwemy",
-        "orderID": "jeremkjy@mail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "2100",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "jer4emy",
-        "orderID": "jeremyew@mail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "2100",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "je2vremy",
-        "orderID": "jeremewy@mail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "2100",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "jervemy",
-        "orderID": "jeremyfd@mail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "2100",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "jere8my",
-        "orderID": "jeremy@mwail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "2100",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "jereamy",
-        "orderID": "jereytmy@mail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "2100",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "je2remy",
-        "orderID": "jew2dremy@mail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "2100",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "jer3emy",
-        "orderID": "jerem3wdy@mail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "2100",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "jere8my",
-        "orderID": "jeresdmy@mail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "2100",
-        "status": "submitted"
-      },
-      {
-        "buyerName": "jsderemy",
-        "orderID": "jeremfdgy@mail.com",
-        "paymentDateFormatted": "2021-12-16T00:00:00+08:00",
-        "bondValueInUSDFormatted": "2100",
-        "status": "submitted"
-      }
-    ]
-  }
-
-
   render() {
 
-    let ordersList = this._getSampleData();
+    let orders = this.props.orders;
 
     const { ExportCSVButton } = CSVExport;
 
     const columns = [
       {
-        dataField: 'orderID',
+        dataField: 'orderId',
         text: 'Order ID',
         sort: true,
         headerStyle: {
@@ -181,7 +49,7 @@ export default class OrderList extends React.Component {
       },
       {
         dataField: 'bondValueInUSDFormatted',
-        text: 'Total Amount(USD)',
+        text: 'Total Amount (USD)',
         sort: true,
         headerStyle: {
           backgroundColor: '#F8F9FA',
@@ -207,7 +75,7 @@ export default class OrderList extends React.Component {
     }];
 
     const paginationOptions = {
-      sizePerPage: 5,
+      sizePerPage: 10,
       hideSizePerPage: true,
       hidePageListOnlyOnePage: true
     };
@@ -219,8 +87,8 @@ export default class OrderList extends React.Component {
         <div className="table-layout">
           <span className="bond-order-list-header">Bond Order List</span>
           <ToolkitProvider
-            keyField="orderID"
-            data={ordersList}
+            keyField="orderId"
+            data={orders}
             columns={columns}
             exportCSV
             bootstrap4
